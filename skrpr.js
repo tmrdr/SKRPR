281737bc3ac04601d2ad327bdb44455b3b92a16d
Tobias Murphy <murphy.tobias@gmail.com>
2017-01-09 13:52:13 -0800

    title company description and link found by scraper

--- skrpr.js
+++ skrpr.js
@@ -1,27 +1,33 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-var URL = 'http://kexp.org/playlist/2007/1/4/3pm';
+var URL = 'https://www.indeed.com/jobs?q=%22junior+web+developer%22&l=Seattle,+WA&explvl=entry_level';
 
 request(URL, function(err, response, body) {
   var $ = cheerio.load(body);
-  var plays = $(".Play");
+  var jobs = $("#resultsCol .result");
 
-	// iterate through the list of found plays
-  plays.each(function(index, play) {
+	//iterate through the list of found plays
+  jobs.each(function(index, job) {
     // the each function gives us the raw HTML element.
 	  // convert the element back to a cheerio
-    play = $(play);
+    job = $(job);
+  //
+    var title = job.find(".turnstileLink");
+    var company = job.find(".company");
+    var summary = job.find(".summary");
+    var link = job.find("a.turnstileLink");
+  //   var timestamp = play.find(".AirDate span");
+  //   var releasename = play.find(".ReleaseMetadata .ReleaseName");
 
-    var artist = play.find(".ArtistName");
-    var track = play.find(".TrackName");
-    var timestamp = play.find(".AirDate span");
-    var releasename = play.find(".ReleaseMetadata .ReleaseName");
-
-    console.log(artist.text());
-    console.log(track.text());
-    console.log(releasename.text());
-    console.log(timestamp.text());
-    console.log();
+    // console.log(artist.text());
+    // console.log(track.text());
+    // console.log(releasename.text());
+    // console.log(timestamp.text());
+    // console.log(jobs);
+    console.log("Title:", title.text().trim());
+    console.log("Company:", company.text().trim());
+    console.log("Summary:", summary.text().trim());
+    console.log("Link:", link.attr('href').trim());
   });
 });
