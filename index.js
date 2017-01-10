8442799c724262b6076f49a0614d95e8eaa1b199
Tobias Murphy <murphy.tobias@gmail.com>
2017-01-10 11:54:19 -0800

    search function + indeed scraper working

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
+var request = require('request');
+var cheerio = require('cheerio');
 var session = require('express-session');
 var passport = require('./config/ppConfig');
 var flash = require('connect-flash');
@@ -37,6 +39,36 @@ app.get('/', function(req, res) {
 
 app.get('/profile', isLoggedIn, function(req, res) {
   res.render('profile');
+});
+
+app.post('/results', function(req, res){
+  console.log(req.body.search);
+  var URL = 'https://www.indeed.com/jobs?q=' + req.body.search.replace(/ /g,"+") + '&l=Seattle,+WA&explvl=entry_level';
+
+  request(URL, function(err, response, body) {
+    var $ = cheerio.load(body);
+    var jobs = $("#resultsCol .result");
+    var results = [];
+
+  	//iterate through the list of found plays
+    jobs.each(function(index, job) {
+      // the each function gives us the raw HTML element.
+  	  // convert the element back to a cheerio
+      job = $(job);
+    //
+      var title = job.find(".turnstileLink");
+      var company = job.find(".company");
+      var summary = job.find(".summary");
+      var link = job.find("a.turnstileLink");
+
+      console.log("Title:", title.text().trim());
+      console.log("Company:", company.text().trim());
+      console.log("Summary:", summary.text().trim());
+      console.log("Link:", link.attr('href').trim());
+      results.push({title:title.text().trim(), company:company.text().trim(), summary:summary.text().trim(), link:link.attr('href').trim()});
+    });
+    res.render('results', {results:results});
+  });
 });
 
 app.use('/auth', require('./controllers/auth'));
