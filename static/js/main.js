7b23242fc67d9ac64d51ef5b5261e51be521a8ec
Tobias Murphy <murphy.tobias@gmail.com>
2017-01-13 10:49:43 -0800

    alerts stay put, results page does not redirect on job add, duplicate alerts removed, my jobs button added to results page

--- static/js/main.js
+++ static/js/main.js
@@ -1,45 +1,49 @@
-$(document).ready(function(){});
 
+$(document).ready(function(){
+  console.log("DOCUMENT READY JS");
+  $('.saveJob').on('submit', function(ev){
+    console.log("SUBMITTING");
+    ev.preventDefault();
+    // console.log($(ev.target).serialize());
 
+    var element = $(this);
+    var url = element.attr('action');
+    var formData = element.serialize();
+    console.log(url);
+    console.log(formData);
 
+    $.ajax({
+      method: 'POST',
+      url: url,
+      data: formData
+    }).done(function(data){
+      console.log(data);
+      console.log("AJAX RUNNING");
 
+      var html = '<div class="alert alert-success" id="success-alert">' + data + '</div>';
 
-$('#saveJob').on('submit', function(ev){
-  ev.preventDefault();
-  // console.log($(ev.target).serialize());
-
-  var element = $(this);
-  var url = element.attr('action');
-  var formData = element.serialize();
-  console.log(url);
-  console.log(formData);
-
-
-  $.ajax({
-    method: 'POST',
-    url: url,
-    data: formData
-  }).done(function(data){
-    console.log(data);
-    window.location= "/profile";
-
+      $("#alerts-container").append(html).fadeTo(2000, 500).slideUp(500, function(){
+          $("#success-alert").slideUp(500);
+      });
+    });
   });
-});
 
-//delete
-$(".deleteJob").on('submit', function(ev){
-  ev.preventDefault();
-  console.log("Delete just got clicked...");
-  var element = $(this);
-  var url = element.attr('action');
+  //delete
+  $(".deleteJob").on('submit', function(ev){
+    ev.preventDefault();
+    console.log("Delete just got clicked...");
+    var element = $(this);
+    var url = element.attr('action');
 
-  $.ajax({
-    method: 'DELETE',
-    url: url
-  }).done(function(data) {
-    // get data returned from the DELETE route
-    console.log(data);
-    // go back to the homepage after deleting anything.
-    window.location = '/profile';
+    $.ajax({
+      method: 'DELETE',
+      url: url
+    }).done(function(data) {
+      // get data returned from the DELETE route
+      console.log(data);
+      // go back to the homepage after deleting anything.
+      window.location = '/profile';
+    });
   });
+
 });
